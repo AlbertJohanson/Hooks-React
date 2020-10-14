ff0204c6ba62953b4ecac44384f76fba88a82b78
Albert Johanson <alberttruk.11@gmail.com>
2020-10-14 17:47:00 -0600

    useState folder added

---
+++ HookApp.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './src/styles/components/App.css';
+
+const HookApp = () => {
+  return (
+    <div>
+      <h1>Hola React</h1>
+    </div>
+  );
+};
+
+export default HookApp;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
+/* eslint-disable import/no-useless-path-segments */
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/App';
-
-ReactDOM.render(<App />, document.getElementById('app'));
+import  CounterApp from '../src/components/01-useState/useState'
+/* import HookApp from '../HookApp';
+ */
+ReactDOM.render(<CounterApp />, document.getElementById('app'));
